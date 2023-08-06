216262cbf27c05c003805cdc6fc597b8a5ccd16c
natan003 <140882696+natan003@users.noreply.github.com>
2023-08-06 11:49:21 +0300

    robel sisay

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,160 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+class Shape {
+    private String color;
+
+    public Shape(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public void display() {
+        System.out.println("Shape: " + getClass().getSimpleName() + ", Color: " + color);
+    }
+
+    public double calculateArea() {
+        return 0.0;
+    }
+
+    public double calculatePerimeter() {
+        return 0.0;
+    }
+}
+
+class Circle extends Shape {
+    private double radius;
+
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+
+    public void display() {
+        System.out.println("Shape: Circle, Color: " + getColor());
+    }
+
+
+    public double calculateArea() {
+        return Math.PI * radius * radius;
+    }
+
+
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle extends Shape {
+    private double length;
+    private double width;
+
+    public Rectangle(String color, double length, double width) {
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+
+    public void display() {
+        System.out.println("Shape: Rectangle, Color: " + getColor());
+    }
+
+
+    public double calculateArea() {
+        return length * width;
+    }
+
+
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Triangle extends Shape {
+    private double base;
+    private double height;
+
+    public Triangle(String color, double base, double height) {
+        super(color);
+        this.base = base;
+        this.height = height;
+    }
+
+    public double getBase() {
+        return base;
+    }
+
+    public void setBase(double base) {
+        this.base = base;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+
+    public void display() {
+        System.out.println("Shape: Triangle, Color: " + getColor());
+    }
+
+
+    public double calculateArea() {
+        return 0.5 * base * height;
+    }
+
+
+    public double calculatePerimeter() {
+        // Assuming it's an equilateral triangle
+        return 3 * base;
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-               System.out.println("hello");
+        Shape[] shapes = new Shape[3];
+        shapes[0] = new Circle("Red", 12);
+        shapes[1] = new Rectangle("yellow", 6, 7);
+        shapes[2] = new Triangle("Green", 6, 9);
+
+        for (Shape shape : shapes) {
+            shape.display();
+            System.out.println("Area: " + shape.calculateArea());
+            System.out.println("Perimeter: " + shape.calculatePerimeter());
+            System.out.println();
         }
     }
+}
